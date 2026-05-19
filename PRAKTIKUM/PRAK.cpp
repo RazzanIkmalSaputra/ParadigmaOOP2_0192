51f3bb72245c1e627e3c086d2a3c205189e01e3b
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:44:28 +0700

    bikin array pointer dari base class di main

--- PRAKTIKUM/PRAK.cpp
+++ PRAKTIKUM/PRAK.cpp
@@ -1,6 +1,10 @@
 
-  
+
+int main() {
+    RekeningBank *daftar[4];
+    daftar[0] = new RekeningSyariah(8000000);
+    daftar[1] = new RekeningKonvensional(1200000);
+    daftar[2] = new RekeningPremium(20000000);
+    daftar[3] = new RekeningPremium(8000000);
+
     
-    string jenis() const override {
-        return "Rekening Premium";
-    }
