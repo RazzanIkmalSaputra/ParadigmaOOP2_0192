6903961a90008bf24742c8a302d4b687008379f6
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:12:31 +0700

    update fungsi main biar sekalian manggil objek lia

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,17 +1,7 @@
 
 
-int main() {
-    seseorang* obyek;
-    joko a;
-    lia b;
-
-    // Menunjuk ke objek joko
-    obyek = &a;
-    obyek->pesan();
-
     // Menunjuk ke objek lia
     obyek = &b;
     obyek->pesan();
 
-    return 0;
-}
+\
