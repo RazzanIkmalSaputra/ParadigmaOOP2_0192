74097129e8c0c34e147ead32dc5222dcdf95eb6e
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:11:54 +0700

    bikin logic main pointer buat manggil si joko

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,9 +1,17 @@
 
 
-class lia : public seseorang {
-public:
-    void pesan() override {
-        cout << "Pesan dari lia" << endl;
-    }
-};
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
 
+    // Menunjuk ke objek joko
+    obyek = &a;
+    obyek->pesan();
+
+    // Menunjuk ke objek lia
+    obyek = &b;
+    obyek->pesan();
+
+    return 0;
+}
