7dac9ab98ffe18b09dfc72d3ed838a9de35f5f8e
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:17:46 +0700

    running tes fungsi perkenalan1 di main

--- noOveriding.cpp
+++ noOveriding.cpp
@@ -1,10 +1,7 @@
 
+int main() {
+    derivedClass a;
+    a.perkenalan1();
 
-class derivedClass : public baseClass {
-public:
-    // Menggunakan nama fungsi yang berbeda karena perkenalan() sudah final
-    void perkenalan1() {
-        cout << "Halo saya Function dari derived Class" << endl;
-    }
-};
-
+    return 0;
+}
