b4904322d07a04016f5e93b8d1923fa2039a41b1
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:17:14 +0700

    bikin fungsi baru perkenalan1 di derived class biar ga error

--- noOveriding.cpp
+++ noOveriding.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
 
-using namespace std;
 
-class baseClass {
+class derivedClass : public baseClass {
 public:
-    // Fungsi ini tidak bisa di-override lagi di derivedClass
-    virtual void perkenalan() final {
-        cout << "Halo saya Function dari base class" << endl;
+    // Menggunakan nama fungsi yang berbeda karena perkenalan() sudah final
+    void perkenalan1() {
+        cout << "Halo saya Function dari derived Class" << endl;
     }
 };
 
