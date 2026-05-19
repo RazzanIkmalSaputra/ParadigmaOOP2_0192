24c66a37a4b597a4d54599fe6e68d933f2c07168
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:09:14 +0700

    panggil class di main terus tes running

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,23 +1,9 @@
-#include <iostream>
-#include <string>
 
-using namespace std;
 
-class AbstraksiKlas {
-private:
-    string x, y;
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
 
-public: 
-    // Method untuk mengisi nilai private member
-    void setXY(string a, string b) {
-        x = a;
-        y = b;
-    }
-
-    // Method untuk menampilkan nilai
-    void display() {
-        cout << "x = " << x << endl;
-        cout << "y = " << y << endl;
-    }
-};
-
+    return 0;
+}
