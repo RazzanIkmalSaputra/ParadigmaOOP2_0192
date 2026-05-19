8fdf7881c46aa8f0cf4109882255107e12ef5cc1
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:19:06 +0700

    tambah fungsi getter setter saklar plus validasi batas array

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
-#include <string>
 
-using namespace std;
 
-class remoteLampu {
-private:
-    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        if (i >= 0 && i < 10) {
+            saklarNo[i] = value;
+        }
+    }
 
+    string getSaklarNo(int i) {
+        if (i >= 0 && i < 10) {
+            return saklarNo[i];
+        }
+        return "Index di luar jangkauan";
+    }
