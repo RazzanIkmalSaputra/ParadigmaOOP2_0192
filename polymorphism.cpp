da1510074c380c25c9222a238c4da1befd4ee062
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:10:04 +0700

    bikin parent class seseorang pake virtual function

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -8,32 +8,3 @@ public:
     virtual void pesan() = 0; 
 };
 
-class joko : public seseorang {
-public:
-    void pesan() override {
-        cout << "Pesan dari joko" << endl;
-    }
-};
-
-class lia : public seseorang {
-public:
-    void pesan() override {
-        cout << "Pesan dari lia" << endl;
-    }
-};
-
-int main() {
-    seseorang* obyek;
-    joko a;
-    lia b;
-
-    // Menunjuk ke objek joko
-    obyek = &a;
-    obyek->pesan();
-
-    // Menunjuk ke objek lia
-    obyek = &b;
-    obyek->pesan();
-
-    return 0;
-}
