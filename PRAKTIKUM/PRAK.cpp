4f7ceb8af974b7133f72bd560486e17afbe79054
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-19 11:42:28 +0700

    bikin class RekeningPremium set batas bebas biaya

--- PRAKTIKUM/PRAK.cpp
+++ PRAKTIKUM/PRAK.cpp
@@ -1,5 +1,8 @@
 
-    
-    string jenis() const override {
-        return "Rekening Konvensional";
-    }
+
+class RekeningPremium : public RekeningBank {
+public:
+    static const long long BATAS_BEBAS = 10000000;
+    static const long long BIAYA_ADMIN = 50000;
+    RekeningPremium(long long awalSaldo) : RekeningBank(awalSaldo) {}
+   
